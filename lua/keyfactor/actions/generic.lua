0ded22289525fc3003005e256cf313fc2aa6c95a
John Wilmes <jw@johnwilmes.name>
2023-03-10 16:44:39 -0600

    scroll action for new view structure

--- lua/keyfactor/actions/generic.lua
+++ lua/keyfactor/actions/generic.lua
@@ -7,40 +7,94 @@ local module = {}
 --[[
 
     mode
-    reverse (boolean)
-    partial (boolean)
-    linewise (boolean)
+    view = index of mode viewport? if not specified, use first viewport that recommends targetting
+        in appropriate scroll direction
+    direction = up/down/left/right
+    count
+    pagewise: true/false or "partial"; default "partial"
+
+    if viewport is not currently visible in a particular window, then
+        only linewise=true, partial=false is permitted; if anything else is set, then no scrolling
 
 --]]
-module.scroll = kf.binding.api(function(params)
-    local mode = kf.fill(params, "mode")
-    local target
-    for _,t in ipairs(mode:get_targets()) do
-        if t.name and t.viewport and t.multiline then
-            target = t
+local direction_to_axis = {up="vertical", down="vertical", left="horizontal", right="horizontal"}
+
+-- direction:  pagewise
+local cmd_index = {
+    up = {
+        [false] = "<C-y>",
+        partial = "<C-u>",
+        [true] = "<C-b>",
+    }
+    down = {
+        [false] = "<C-e>",
+        partial = "<C-d>",
+        [true] = "<C-f>",
+    },
+    left = {
+        [false] = "zh",
+        partial = "zH",
+        [true] = "zH", -- also multiply count by 2
+    },
+    right = {
+        [false] = "zl",
+        partial = "zL",
+        [true] = "zL", -- also multiply count by 2
+    },
+}
+
+module.scroll = kf.binding.action(function(params)
+    local mode, count = kf.fill(params, "mode", "count")
+    if not mode.view then
+        -- TODO log warning?
+        return
+    end
+    local direction = params.direction
+    local axis = direct_to_axis[direction]
+    if not axis then
+        direction="down"
+        axis="vertical"
+    end
+
+    local pagewise = params.pagewise
+    if type(pagewise)~="boolean" then
+        pagewise="partial"
+    end
+
+    local cmd = cmd_index[direction][pagewise]
+    if axis=="horizontal" and pagewise==true then
+        count = count*2
+    end
+    if count > 1 then
+        cmd = count..cmd
+    end
+
+    local viewports = mode.view:get_viewports()
+    local index = params.view
+    local viewport = viewports[index]
+
+    if not viewport then
+    -- iterate over viewports and select the first one that supports scrolling in this direction
+    -- ask the window manager if that viewport is actually present somewhere
+        -- if so, apply native vim scrolling in that window, get vim.fn.winsaveview,
+            -- and call view:set with the result
+        -- otherwise, for pagewise scroll, just guess the number of lines
+
+        for i,v in ipairs(mode.view:get_viewports()) do
+            if v.scroll==axis or v.scroll=="both" then
+                index = i
+                viewport = v
+                break
+            end
         end
     end
-    if not target then
+
+    if not viewport then
+        -- TODO log warning
         return
     end
 
-    local viewport = target.viewport
-    local reverse = not not params.reverse
-    if params.linewise or params.partial then
-        local count = 1
-        if params.partial then
-            count = vim.api.nvim_win_get_option(target.window, 'scroll')
-        end
-        viewport = kf.viewport.scroll_lines(target.viewport, reverse, count)
-    else
-        viewport = kf.viewport.scroll_pages(target.viewport, reverse, 1)
-    end
-
-    mode[target.name]:set_details{
-        window=target.window,
-        buffer=target.buffer,
-        viewport=viewport
-    }
+    kf.scroll(view, index, cmd)
 end)
 
 
---
+++ lua/keyfactor/api/scroll.lua
@@ -0,0 +1,32 @@
+local function scroll(view, viewport, action)
+    local callable
+    local arg
+    if type(action)=="string" then
+        -- action assumed to be normal mode command
+        callable = vim.cmd
+        arg = "normal! "..vim.api.nvim_replace_termcodes(action, true, true, true)
+    elseif utils.is_callable(action) then
+        callable = action
+        arg = nil
+    else
+        error("invalid scroll action")
+    end
+
+    local wm = kf.get_window_manager()
+    local window = wm.get_window(view, viewport)
+    if not window then
+        -- TODO log message, can't scroll without window
+        return
+    end
+
+    local saveview
+    vim.api.nvim_win_call(window, function()
+        -- we are just assuming that the view is rendered properly in the window
+        callable(arg)
+        saveview = vim.fn.winsaveview()
+    end)
+
+    view.set{[viewport]={view=saveview}}
+end
+
+return {scroll=scroll}
