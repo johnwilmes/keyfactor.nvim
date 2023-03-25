63211a31189fb7c9618b65549457bc8f6bc22c96
John Wilmes <jw@johnwilmes.name>
2023-03-24 22:33:44 -0500

    more conservative page layout updates and redrawing

--- lua/keyfactor/api/view/page.lua
+++ lua/keyfactor/api/view/page.lua
@@ -16,24 +16,53 @@ local function reset_tab(tab)
     end)
 end
 
-local function do_layout(layout, windows, win)
-    vim.api.nvim_set_current_win(win)
-    if not (layout.mode or (layout.orientation and layout[1] and layout[2])) then
-        return
+local function fix_layout(current, target, modes, windows)
+    if #current~=#target+1 then
+        -- 1. produce a normal "base" window
+        local windows = utils.list.filter(vim.tbl_flatten(current), function(x)
+            return type(x)=="number"
+        end)
+        local base_win
+        for i=1,#windows do
+            local win = windows[i]
+            if vim.fn.win_gettype(win)=="" then
+                base_win=win
+                break
+            end
+        end
+        if not base_win then
+            vim.api.nvim_set_current_win(windows[1])
+            vim.cmd(target.split.." split")
+            base_win=vim.api.nvim_get_current_win()
+        end
+        -- 2. close everything under `current` except for the base window
+        for _,win in ipairs(windows) do
+            if win~=base_win then
+                vim.api.nvim_win_hide(win)
+            end
+        end
+        -- 3. split the base window to the cardinality of target
+        --    update `current` to match resulting structure
+        if #target > 1 then
+            current = {(target.split=="vertical" and "row") or "col", {"leaf", base_win}}
+            for i=#target-1 do
+                vim.cmd(target.split.." rightbelow split")
+                current[i+2] = {"leaf", vim.api.nvim_get_current_win()}
+            end
+        else
+            current = {"leaf", base_win}
+        end
     end
+
     if layout.mode then
-        local record = {window=win, mode=layout.mode}
-        windows[layout.mode] = record
+        local win = current[2]
+        local record = {window=win, mode=layout.mode, stale={size=true, buffer=true, viewport=true}}
+        modes[layout.mode] = record
         windows[win] = record
-        return
-    end
-    local split = {win}
-    for i=2,#split do
-        vim.cmd(layout.orientation.." split")
-        split[i]=vim.api.nvim_get_current_win()
-    end
-    for i,w in ipairs(split) do
-        do_layout(layout[i], windows, w)
+    else
+        for i,w in ipairs(target) do
+            fix_layout(current[i+1], target[i], modes, windows)
+        end
     end
 end
 
@@ -51,12 +80,20 @@ function Page:_start(tab)
     assert(not self._tab, "page is already started")
     assert(self==kf.get_page(tab), "inconsistent page state")
     self._tab = tab
-    self._base_win = reset_tab(tab)
-    self._is_tabpage_stale = true
-    self._layout.resize()--TODO
+    reset_tab(tab)
+    self._layout.resize()--TODO notify layout of page dimensions
     self._modes = {}
+    self._windows = {}
 
+    -- TODO probably these autocmds should be set up once and used for all pages - easier to
+    -- dispatch
+    self._tab_autocmd = vim.api.nvim_create_autocmd("TabLeave", ...)
+    self._layout_autocmd = vim.api.nvim_create_autocmd({"WinClosed", "WinNew"}, {
+    })
+    self._scroll_autocmd = vim.api.nvim_create_autocmd("WinScrolled", {
+    })
     -- TODO start listening for window events
+    self._is_tabpage_stale = true
 end
 
 function Page:_stop(tab)
@@ -97,62 +134,74 @@ end
 
 function Page:draw()
     utils.vim.noautocmd(function()
+        -- Tabpage displayed
         if self._is_tabpage_stale then
             vim.api.nvim_set_current_tabpage(self._tab)
         end
-        local modes = kf.get_attached_modes(self)
-        -- TODO handle mismatch between get_attached_modes and added modes?
+
+        -- Window layout
         if self._is_layout_stale then
-            self._base_win = reset_tab(self._tab)
-            if #modes>0 then
-                self._windows = {}
-                local layout = self._layout:get_splits()
-                do_layout(layout, self._windows, self._base_win)
-                self._base_win = nil
-            end
+            local current = vim.fn.winlayout(vim.api.nvim_tabpage_get_number(self._tab))
+            local target = self._layout:get_splits()
+            self._modes = {}
+            self._windows = {}
+            fix_layout(current, target, self._modes, self._windows)
             self._is_layout_stale = nil
-            self._is_size_stale = true
-        end
-        if self._is_size_stale then
-            local sizes = self._layout:get_sizes()
-            for win,_ in self._windows do
-                if type(win)=="number" then
-                    vim.api.nvim_win_option(win, "winfixwidth", false)
-                    vim.api.nvim_win_option(win, "winfixheight", false)
-                end
-            end
-            for win,record in self._windows do
-                if type(win)=="number" then
-                    local mode = record.mode
-                    self._is_mode_stale[mode]=true
-                    local s = sizes[mode]
-                    if s then
-                        vim.api.nvim_set_current_win(win)
-                        vim.call("vertical resize "..s.width) -- yes, vertical resize changes width
-                        vim.call("horizontal resize "..s.height)
-                        vim.api.nvim_win_option(win, "winfixwidth", true)
-                        vim.api.nvim_win_option(win, "winfixheight", true)
-                    end
-                end
-            end
-            self._is_size_stale = nil
             self._is_focus_stale = true
         end
-        for mode,_ in pairs(self._is_mode_stale) do
-            local win = self._windows[mode].window
-            local view = mode.view
-            -- TODO validate
-            vim.api.nvim_win_set_buf(win, view.buffer)
-            -- TODO set view.viewport
-            -- TODO set view.options?
-            -- TODO set view.config style options?
-            view:draw(win)
-            self._is_mode_stale[mode]=nil
+
+        -- Window sizes
+        local size_stale = {}
+        local sizes = self._layout:get_sizes()
+        for mode,record in pairs(self._modes) do
+            local window = record.window
+            local height = vim.api.nvim_win_get_height(window)
+            local width = vim.api.nvim_win_get_width(window)
+            if record.stale.size and (sizes[mode].height~=height or sizes[mode].width~=width) then
+                size_stale[#size_stale]=record
+                vim.api.nvim_win_option(window, "winfixwidth", false)
+                vim.api.nvim_win_option(window, "winfixheight", false)
+            else
+                vim.api.nvim_win_option(window, "winfixwidth", true)
+                vim.api.nvim_win_option(window, "winfixheight", true)
+            end
+            record.stale.size = nil
         end
+        for _,record in ipairs(stale_size) do 
+            vim.api.nvim_win_set_width(record.window, sizes[record.mode].width)
+            vim.api.nvim_win_set_height(record.window, sizes[record.mode].height)
+            vim.api.nvim_win_option(window, "winfixwidth", true)
+            vim.api.nvim_win_option(window, "winfixheight", true)
+            record.stale.viewport = true
+        end
+
+        -- Window buffer, scrolling, and drawing
+        for mode,record in pairs(self._modes) do
+            local view = mode.view
+            local window = record.window
+            if record.stale.buffer then
+                vim.api.nvim_win_set_buf(window, view.buffer)
+                record.stale.buffer = nil
+                record.stale.viewport = true
+            end
+            if record.stale.viewport then
+                vim.api.nvim_win_call(window, function()
+                    vim.fn.winrestview(view.viewport)
+                end)
+                record.stale.viewport = nil
+                record.stale.draw = true
+            end
+            if record.stale.draw then
+                view:draw(window)
+                record.stale.draw = nil
+            end
+        end
+
+        -- Set focus window
         if self._is_focus_stale then
             local mode = kf.mode.get_mode(self)
             if mode then
-                vim.api.nvim_set_current_win(self._windows[mode].window)
+                vim.api.nvim_set_current_win(self._modes[mode].window)
             end
             self._is_focus_stale = nil
         end
